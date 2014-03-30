80f0e3bc603459cebdd1fc61ae4da42c7098ddc1
Anton Iermolenko <inflyer@hotmail.com>
2014-03-30 07:56:26 +0300

    Support multi-population measures reporting

--- lib/health-data-standards/models/cqm/query_cache.rb
+++ lib/health-data-standards/models/cqm/query_cache.rb
@@ -18,8 +18,8 @@ module HealthDataStandards
       field :OBSERV, type: Float
       field :supplemental_data, type: Hash
 
-      def self.aggregate_measure(measure_id, effective_date, test_id=nil)
-        cache_entries = self.where(effective_date: effective_date, measure_id: measure_id, test_id: test_id)
+      def self.aggregate_measure(measure_id, sub_id, effective_date, test_id=nil)
+        cache_entries = self.where(effective_date: effective_date, sub_id: sub_id, measure_id: measure_id, test_id: test_id)
         aggregate_count = AggregateCount.new
         aggregate_count.measure_id = measure_id
         cache_entries.each do |cache_entry|
